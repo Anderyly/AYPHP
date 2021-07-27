bde986fbaf50b5cb6b4860c8967c08f28c2dbc22
anderyly <admin@aaayun.cc>
2021-07-27 13:26:12 +0800

    更新

---
+++ app/controller/Index.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace app\index\controller;
+
+class Index
+{
+    public function index() {
+        success('欢迎使用AYPHP框架');
+    }
+}
---
+++ public/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @author anderyly
+ * @email admin@aaayun.cc
+ * @link http://vclove.cn/
+ * @copyright Copyright (c) 2018
+ */
+
+echo 111;exit;
+define('APP_NAME', 'app');
+
+echo 11;exit;
+include "../ay/ay.php";
