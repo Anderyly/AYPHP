56c3dbaf373e70b39f476cab5bcf76c48675d97d
anderyly <admin@aaayun.cc>
2022-06-16 14:34:50 +0800

    水印图片

--- config/database.php
+++ config/database.php
@@ -2,7 +2,7 @@
 /**
  * @author anderyly
  * @email admin@aaayun.cc
- * @link http://vclove.cn/
+ * @link http://blog.aaayun.cc
  * @copyright Copyright (c) 2018
  */
 
@@ -12,6 +12,6 @@ return [
     'DB_PORT' => 3306,
     'DB_USER' => 'root',
     'DB_PASS' => 'root',
-    'DB_NAME' => 'ay',
+    'DB_NAME' => 'oa',
     'DB_PRE'  => ''
 ];
